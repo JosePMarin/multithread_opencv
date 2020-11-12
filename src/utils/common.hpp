4141b8ec5cc5649f64218554ae6f43636196acdd
josePMarin <josepablomarinvalera@gmail.com>
2020-11-12 16:21:13 +0100

    deleted old main

--- src/utils/common.hpp
+++ src/utils/common.hpp
@@ -17,3 +17,26 @@
 #include "opencv2/opencv.hpp"
 
 #include "object_queue.hpp"
+
+
+std::chrono::_V2::system_clock::time_point check_time(std::chrono::_V2::system_clock::time_point *start=nullptr,
+							std::chrono::_V2::system_clock::time_point *end=nullptr,
+							const char *process=nullptr
+							)
+{
+	std::chrono::_V2::system_clock::time_point time;
+	if (start==nullptr && end == nullptr)
+	{
+		time = std::chrono::system_clock::now();
+	} 
+	if (process!=nullptr)
+	{
+		std::string str(process);
+		std::chrono::duration<double> elapsed_seconds = *end-*start;
+		std::time_t end_time = std::chrono::system_clock::to_time_t(*end);
+
+		std::cout << "finished "<< str << " at " << std::ctime(&end_time)
+				<< "elapsed time: " << elapsed_seconds.count() << "s\n";
+	}
+	return time;
+}
